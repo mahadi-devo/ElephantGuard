7174b0fa292bf349fef24065864ac47cf4c790c9
mahadi-devo <mahadihassan083@gmail.com>
2020-10-31 08:58:03 +0530

    Initial Commit

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Elephant.java
@@ -0,0 +1,25 @@
+package org.tensorflow.lite.examples.classification;
+
+public class Elephant {
+
+    private String lat;
+    private String lon;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public void Elephant (){};
+}
